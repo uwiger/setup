6c2efd014f5eb2dec05acc32add0916d2d2ef995
Ulf Wiger <ulf@feuerlabs.com>
2012-03-06 14:16:53 +0100

    Add setup:patch_app(A) for patching an application in embedded mode
    This function uses ERL_LIBS to find the 'development path' to the
    application A, and prepends the erlang code path with this directory.
    This can be useful when running a test system built using rebar/reltool,
    and starting with embedded code loading against a separate code tree.

--- src/setup.erl
+++ src/setup.erl
@@ -27,9 +27,12 @@
          data_dir/0,
          verify_directories/0,
          verify_dir/1,
-         find_env_vars/1]).
+         find_env_vars/1,
+         patch_app/1]).
 
 -export([run_setup/2]).
+
+-include_lib("kernel/include/file.hrl").
 
 %% @spec start(Type, Args) -> {ok, pid()}
 %% @doc Application start function.
@@ -158,6 +161,24 @@ env_value("HOME") -> home().
 env_value("APP", A) -> atom_to_list(A);
 env_value("PRIV_DIR", A) -> code:priv_dir(A);
 env_value("LIB_DIR" , A) -> code:lib_dir(A).
+
+patch_app(A) ->
+    CurLibDir = code:lib_dir(A),
+    [A1|_] = lists:reverse(filename:split(CurLibDir)),
+    ADir = case re:split(A1, "-", [{return, list}]) of
+               [_] -> A1;
+               [AD,_] -> AD
+           end,
+    RevPfx = lists:reverse(filename:join(ADir, "ebin")),
+    LibDirs = get_user_lib_dirs(),
+    case [P || P <- LibDirs,
+               lists:prefix(RevPfx, lists:reverse(P))] of
+        [Found|_] ->
+            io:fwrite("code:add_patha(~s)~n", [Found]),
+            code:add_patha(Found);
+        _ ->
+            {error, not_found}
+    end.
 
 
 %% @hidden
@@ -292,3 +313,168 @@ all_included([H | T]) ->
     end;
 all_included([]) ->
     [].
+
+
+
+
+%% stolen from code_server.erl:
+get_user_lib_dirs() ->
+    case os:getenv("ERL_LIBS") of
+        LibDirs0 when is_list(LibDirs0) ->
+            Sep =
+                case os:type() of
+                    {win32, _} -> $;;
+                    _          -> $:
+                end,
+            LibDirs = split_paths(LibDirs0, Sep, [], []),
+            get_user_lib_dirs_1(LibDirs);
+        false ->
+            []
+    end.
+
+get_user_lib_dirs_1([Dir|DirList]) ->
+    case erl_prim_loader:list_dir(Dir) of
+        {ok, Dirs} ->
+            {Paths,_Libs} = make_path(Dir, Dirs),
+            %% Only add paths trailing with ./ebin.
+            [P || P <- Paths, filename:basename(P) =:= "ebin"] ++
+                get_user_lib_dirs_1(DirList);
+        error ->
+            get_user_lib_dirs_1(DirList)
+    end;
+get_user_lib_dirs_1([]) -> [].
+
+split_paths([S|T], S, Path, Paths) ->
+    split_paths(T, S, [], [lists:reverse(Path) | Paths]);
+split_paths([C|T], S, Path, Paths) ->
+    split_paths(T, S, [C|Path], Paths);
+split_paths([], _S, Path, Paths) ->
+    lists:reverse(Paths, [lists:reverse(Path)]).
+
+
+make_path(BundleDir, Bundles0) ->
+    Bundles = choose_bundles(Bundles0),
+    make_path(BundleDir, Bundles, [], []).
+
+choose_bundles(Bundles) ->
+    ArchiveExt = archive_extension(),
+    Bs = lists:sort([create_bundle(B, ArchiveExt) || B <- Bundles]),
+    [FullName || {_Name,_NumVsn,FullName} <-
+                     choose(lists:reverse(Bs), [], ArchiveExt)].
+
+create_bundle(FullName, ArchiveExt) ->
+    BaseName = filename:basename(FullName, ArchiveExt),
+    case split(BaseName, "-") of
+        [_, _|_] = Toks ->
+            VsnStr = lists:last(Toks),
+            case vsn_to_num(VsnStr) of
+                {ok, VsnNum} ->
+                    Name = join(lists:sublist(Toks, length(Toks)-1),"-"),
+                    {Name,VsnNum,FullName};
+                false ->
+                    {FullName,[0],FullName}
+            end;
+        _ ->
+            {FullName,[0],FullName}
+    end.
+
+%% Convert "X.Y.Z. ..." to [K, L, M| ...]
+vsn_to_num(Vsn) ->
+    case is_vsn(Vsn) of
+        true ->
+            {ok, [list_to_integer(S) || S <- split(Vsn, ".")]};
+        _  ->
+            false
+    end.
+
+is_vsn(Str) when is_list(Str) ->
+    Vsns = split(Str, "."),
+    lists:all(fun is_numstr/1, Vsns).
+
+is_numstr(Cs) ->
+    lists:all(fun (C) when $0 =< C, C =< $9 -> true; 
+                  (_)                       -> false
+              end, Cs).
+
+split(Cs, S) ->
+    split1(Cs, S, []).
+
+split1([C|S], Seps, Toks) ->
+    case lists:member(C, Seps) of
+        true -> split1(S, Seps, Toks);
+        false -> split2(S, Seps, Toks, [C])
+    end;
+split1([], _Seps, Toks) ->
+    lists:reverse(Toks).
+
+split2([C|S], Seps, Toks, Cs) ->
+    case lists:member(C, Seps) of
+        true -> split1(S, Seps, [lists:reverse(Cs)|Toks]);
+        false -> split2(S, Seps, Toks, [C|Cs])
+    end;
+split2([], _Seps, Toks, Cs) ->
+    lists:reverse([lists:reverse(Cs)|Toks]).
+
+join([H1, H2| T], S) ->
+    H1 ++ S ++ join([H2| T], S);
+join([H], _) ->
+    H;
+join([], _) ->
+    [].
+
+choose([{Name,NumVsn,NewFullName}=New|Bs], Acc, ArchiveExt) ->
+    case lists:keyfind(Name, 1, Acc) of
+        {_, NV, OldFullName} when NV =:= NumVsn ->
+            case filename:extension(OldFullName) =:= ArchiveExt of
+                false ->
+                    choose(Bs,Acc, ArchiveExt);
+                true ->
+                    Acc2 = lists:keystore(Name, 1, Acc, New),
+                    choose(Bs,Acc2, ArchiveExt)
+            end;
+        {_, _, _} ->
+            choose(Bs,Acc, ArchiveExt);
+        false ->
+            choose(Bs,[{Name,NumVsn,NewFullName}|Acc], ArchiveExt)
+    end;
+choose([],Acc, _ArchiveExt) ->
+    Acc.
+
+make_path(_,[],Res,Bs) ->
+    {Res,Bs};
+make_path(BundleDir,[Bundle|Tail],Res,Bs) ->
+    Dir = filename:append(BundleDir,Bundle),
+    Ebin = filename:append(Dir,"ebin"),
+    %% First try with /ebin
+    case erl_prim_loader:read_file_info(Ebin) of
+        {ok,#file_info{type=directory}} ->
+            make_path(BundleDir,Tail,[Ebin|Res],[Bundle|Bs]);
+        _ ->
+            %% Second try with archive
+            Ext = archive_extension(),
+            Base = filename:basename(Dir, Ext),
+            Ebin2 = filename:join([filename:dirname(Dir), Base ++ Ext, Base, "ebin"]),
+            Ebins = 
+                case split(Base, "-") of
+                    [_, _|_] = Toks ->
+                        AppName = join(lists:sublist(Toks, length(Toks)-1),"-"),
+                        Ebin3 = filename:join([filename:dirname(Dir), Base ++ Ext, AppName, "ebin"]),
+                        [Ebin3, Ebin2, Dir];
+                    _ ->
+                        [Ebin2, Dir]
+                end,
+            try_ebin_dirs(Ebins,BundleDir,Tail,Res,Bundle, Bs)
+    end.
+
+try_ebin_dirs([Ebin | Ebins],BundleDir,Tail,Res,Bundle,Bs) ->
+    case erl_prim_loader:read_file_info(Ebin) of
+        {ok,#file_info{type=directory}} -> 
+            make_path(BundleDir,Tail,[Ebin|Res],[Bundle|Bs]);
+        _ ->
+            try_ebin_dirs(Ebins,BundleDir,Tail,Res,Bundle,Bs)
+    end;
+try_ebin_dirs([],BundleDir,Tail,Res,_Bundle,Bs) ->
+    make_path(BundleDir,Tail,Res,Bs).
+
+archive_extension() ->
+    init:archive_extension().
